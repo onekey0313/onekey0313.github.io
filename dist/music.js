a945658057d410d03d66dcdcdabee7eb74370b35
onekey0313 <3012234939@qq.com>
2018-09-08 19:00:42 +0800

    Site updated: 2018-09-08 19:00:41

--- dist/music.js
+++ dist/music.js
@@ -6,7 +6,7 @@ const ap = new APlayer({
       {
         name: "Lemmon",
         artist: '米津玄师',
-        url: 'https://m10.music.126.net/20180908173054/7d6b2c1a8695a99641139adf06e0f2f4/ymusic/6eed/4ef2/d8ae/b85b958a70552f7e002e33d35075086c.mp3',
+        url: 'http://isure.stream.qqmusic.qq.com/C400000akynZ2Rbro5.m4a?vkey=667DDD515A8BE7D766D642C2075530C84B9E0DDDADF0C17B7C221734C310BF131C83E03311433C309D33237475F7A209A756FB9B1F94DE9B&guid=6172684334&uin=3012234939&fromtag=66',
         cover: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536406773548&di=6cac2da9cdacdd182a6473e4d11555a9&imgtype=0&src=http%3A%2F%2Foss.tan8.com%2Fyuepuku%2F118%2F59343%2F59343_prev.jpg%3Fv%3D1524962090',
       },
       {
